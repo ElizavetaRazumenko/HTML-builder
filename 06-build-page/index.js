553d6fbd5f5ecc324d98c4d3e2b3127f383f4aa2
ElizavetaRazumenko <razumenko99@mail.ru>
2023-05-08 22:39:11 +0300

    feat: 6 task solve

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,47 +1,95 @@
-const fs = require('fs');
-const fsProm = fs.promises;
 const path = require('path');
-const dist = path.join(__dirname, 'project-dist');
-const slylesPath = path.join(__dirname, 'styles');
-const assetsPath = path.join(__dirname, 'assets');
 
-(function createDistDir() {
-    fs.kdir(path.join(__dirname, 'project-dist'), {
-        recursive: true,
-    }, err => {
-        if (err) {
-            throw new Error('Folder exist');
-        }
+
+const fs = require('fs');
+
+
+const distPath = path.join(__dirname, 'project-dist');
+
+
+async function html_builder(tmpPath) {
+
+  let tmpData = await fs.promises.readFile(tmpPath, 'utf-8'); 
+
+  
+  const componentsPath = path.join(__dirname, 'components');
+
+  
+  const newData = tmpData.match(/\{\{(.+?)\}\}/g)
+                        .map(async (match) => {
+
+  const component = match.slice(2, -2) + '.html';  
+  const componentPath = path.join(componentsPath, component);
+  const componentData = await fs.promises.readFile(componentPath, 'utf-8'); 
+
+  tmpData = tmpData.replace(match, componentData);
+    
+  });
+
+
+  await Promise.all(newData);
+
+  const indexPath = path.join(__dirname, 'project-dist', 'index.html');
+  await fs.promises.writeFile(indexPath, tmpData);
+}
+
+
+
+async function assetsCopy(assets, ultimate = path.join(distPath, 'assets'), outside = true) {
+  if (outside)
+    await fs.promises.rm(ultimate, { 
+      recursive: true, force: true 
     });
-})();
 
-(function createMarkupFile() {
-    const input = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
-    const output = fs.createWriteStream(path.join(dist, 'index.html'));
-    let str = '';
-    input.on('data', data => {
-        str = data.toString();
+  await fs.promises.mkdir(ultimate, { 
+    ecursive: true 
+  });
 
-        function mapper(element) {
-            return `${{element}}`;
-        }
+  const assetsFiles = await fs.promises.readdir(assets); 
 
-        const componentsPath = path.join(__dirname, 'components');
+  for (const assFile of assetsFiles) {
 
-        fs.readdir(
-            componentsPath,
-            { withFileTypes: true },
-            (err, data) => {
-                if (err) {
-                    throw err;
-                }
+    const newPath = path.join(assets, assFile);
+    const stats = await fs.promises.stat(newPath);
 
-                const tmp = [];
-                data.forEach(temp => {
-                    const fileName = temp.name.match(/([\w]*\.)*/)[0].replace('.', '');
-                    tmp.push(mapper(fileName));
-                });
-            }
-        )
-    })
-})
+    if (stats.isDirectory()) {
+      const toPath = path.join(ultimate, assFile); 
+
+      await assetsCopy(newPath, toPath, false);
+    } else {
+
+      await fs.promises.copyFile(newPath, path.join(ultimate, assFile));
+    }
+  }
+}
+
+async function stylesCopy(stylesPath) {
+  const styleText = await fs.promises.readdir(stylesPath); 
+  
+  const files = styleText.filter((smth) => path.extname(smth) === '.css'); 
+
+  let cssData = '';
+
+  for (const file of files) {
+    const filePath = path.join(stylesPath, file);
+    const fileData = await fs.promises.readFile(filePath, 'utf-8');
+    cssData += fileData + '\n';
+  }
+  
+  const outputCSSPath = path.join(distPath, 'style.css');
+
+  await fs.promises.writeFile(outputCSSPath, cssData); 
+}
+
+
+fs.mkdir(distPath, { 
+  recursive: true 
+        }, (err) => {
+  if (err) throw err;
+
+  html_builder(path.join(__dirname, 'template.html'));
+  stylesCopy(path.join(__dirname, 'styles'));
+  assetsCopy(path.join(__dirname, 'assets'));
+
+
+});
